b449049b8cbb141bf551ee305f137a7a6088ec94
Konstantin <iscoulfi@gmail.com>
2023-03-25 17:51:48 +0500

    fix: change app component from function to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
@@ -6,19 +7,21 @@ import AboutUs from './components/pages/AboutUs';
 import NotFound from './components/pages/NotFound';
 import Forms from './components/pages/Forms/Forms';
 
-function App() {
-  return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<AboutUs />} />
-          <Route path="forms" element={<Forms />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<AboutUs />} />
+            <Route path="forms" element={<Forms />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
